f2592ddbf186fdc2d8a5226d567dd47ce2e13f19
pancham01 <pancham9557@gmail.com>
2025-03-18 19:04:32 +0530

    Initial commit

---
+++ src/main/java/com/springcore/Client.java
@@ -0,0 +1,16 @@
+package com.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.springcore.model.Employee;
+
+public class Client {
+
+	public static void main(String[] args) {
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Employee bean = ioc.getBean(Employee.class, "emp");
+		System.out.println(bean);
+	}
+
+}
